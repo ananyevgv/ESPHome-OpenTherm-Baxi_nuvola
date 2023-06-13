7da64b4f3dadc4bc56a7a1ae56cbd9381aa3aa8a
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2023-06-13 11:44:22 +0300

    Update OpenTherm.cpp

--- esphome-opentherm/OpenTherm.cpp
+++ esphome-opentherm/OpenTherm.cpp
@@ -434,7 +434,7 @@ float getHotWaterTemperature() {
     return ot.isValidResponse(response) ? ot.getFloat(response) : 0;
   }
 
-unsigned char OpenTherm::getFault(char Fault) {
+unsigned char OpenTherm::getFault() {
     return ((sendRequest(buildRequest(OpenThermRequestType::READ, OpenThermMessageID::ASFflags, 0)) >> 8) & 0xff);
 }
 unsigned char OpenTherm::getEFault() {
